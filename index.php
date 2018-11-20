97d00141f088a266515fc1b3728ca2a175ff3571
root <personne.nico@gmail.com>
2018-11-20 15:52:24 +0100

    edit

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 1</title>
+</head>
+<body>
 <?php
 $deps = [
-  "02" => "Aisne",
-  "59" => "Nord",
-  "60" => "Oise",
-  "62" => "Pas-de-Calais",
-  "80" => "Somme"
+  '02' => 'Aisne',
+  '59' => 'Nord',
+  '60' => 'Oise',
+  '62' => 'Pas-de-Calais',
+  '80' => 'Somme'
 ];
-foreach($deps as $dep) {
-       echo "<li>" . $dep . "</li>";
-}
+foreach($deps as $dep) {?>
+       <li><?php echo $dep ?></li>
+       <?php
+     }
  ?>
+ <a href="http://PHP/"><button type="button" class="btn btn-danger" name="button">Accueil</button></a>
+</body>
+</html>
